0107f9d02e763b65e5980e24e2342d1cbf11ba25
Netidion <vneti77@gmail.com>
2022-08-08 14:25:11 +0300

    init add of project

---
+++ src/test/java/com/springframwork/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframwork.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
