810ec00e85c4a033dc999ed37a4bf32a395e8249
Netidion <vneti77@gmail.com>
2022-08-12 12:22:00 +0300

    Initialize Database with Spring. Closes #7

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (category_name) VALUES ('Greek');
+INSERT INTO category (category_name) VALUES ('Italian');
+INSERT INTO category (category_name) VALUES ('Mexican');
+INSERT INTO category (category_name) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (uom) VALUES ('teaspoon');
+INSERT INTO unit_of_measure (uom) VALUES ('tablespoon');
+INSERT INTO unit_of_measure (uom) VALUES ('cup');
+INSERT INTO unit_of_measure (uom) VALUES ('gr');
+INSERT INTO unit_of_measure (uom) VALUES ('ml');
