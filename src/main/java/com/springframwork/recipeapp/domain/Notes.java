1e951dbde5bea480ed5cdb8fd841f1f973d3489c
Netidion <vneti77@gmail.com>
2022-08-30 11:33:50 +0300

    Adding Project lombok

--- src/main/java/com/springframwork/recipeapp/domain/Notes.java
+++ src/main/java/com/springframwork/recipeapp/domain/Notes.java
@@ -14,27 +14,67 @@ public class Notes {
     @Lob
     private String notes;
 
+    public Notes() {
+    }
+
     public Long getId() {
-        return id;
+        return this.id;
+    }
+
+    public Recipe getRecipe() {
+        return this.recipe;
+    }
+
+    public String getNotes() {
+        return this.notes;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
     }
 
-    public String getNotes() {
-        return notes;
-    }
-
     public void setNotes(String notes) {
         this.notes = notes;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Notes)) return false;
+        final Notes other = (Notes) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$recipe = this.getRecipe();
+        final Object other$recipe = other.getRecipe();
+        if (this$recipe == null ? other$recipe != null : !this$recipe.equals(other$recipe)) return false;
+        final Object this$notes = this.getNotes();
+        final Object other$notes = other.getNotes();
+        if (this$notes == null ? other$notes != null : !this$notes.equals(other$notes)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Notes;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $recipe = this.getRecipe();
+        result = result * PRIME + ($recipe == null ? 43 : $recipe.hashCode());
+        final Object $notes = this.getNotes();
+        result = result * PRIME + ($notes == null ? 43 : $notes.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Notes(id=" + this.getId() + ", recipe=" + this.getRecipe() + ", notes=" + this.getNotes() + ")";
     }
 }
--- src/main/java/com/springframwork/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/springframwork/recipeapp/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package com.springframwork.recipeapp.services;
 
 import com.springframwork.recipeapp.domain.Recipe;
 import com.springframwork.recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service!");
 
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
